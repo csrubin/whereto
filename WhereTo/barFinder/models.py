4360aeaf4e64e2bf71292ca4cee0087f10fe724d
TreverV <trevervogel@gmail.com>
2018-10-27 19:19:31 -0400

    models.py change

--- WhereTo/barFinder/models.py
+++ WhereTo/barFinder/models.py
@@ -17,17 +17,19 @@ class Establishment(models.Model):
 )
     est_type = models.CharField(max_length=2,
                                 choices=EST_TYPE_CHOICES,blank = True)
-    open_time = models.IntegerField(blank = True)
-    close_time = models.IntegerField(blank = True)
+    open_time = models.CharField(blank = True)
+    close_time = models.CharField(blank = True)
     est_url = models.URLField(blank = True)
+    def __str__(self):
+        return self.establishment_text
     
 class Deals(models.Model):
     est = models.ForeignKey(Establishment,
                                on_delete=models.CASCADE,
                                verbose_name="Establishment FK",
                                )
-    start_time = models.IntegerField(blank = True)
-    end_time = models.IntegerField(blank = True)
+    start_time = models.CharField(blank = True)
+    end_time = models.CharField(blank = True)
     item = models.CharField(max_length = 200,blank = True)
     size = models.CharField(blank = True,max_length = 100)
     cost = models.CharField(max_length = 10,blank = True)
@@ -39,5 +41,8 @@ class Deals(models.Model):
     ('5', 'Friday'),
     ('6', 'Saturday'),
     ('7', 'Sunday'),
+    ('8', 'Every Day')
 )
     day = models.IntegerField(choices = DAY_CHOICES,blank = True)
+    def __str__(self):
+        return self.deals_text
