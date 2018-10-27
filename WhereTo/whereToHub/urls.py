24f737ccb6bdb327de7e869664e8ac0debca1ef6
Ben Memberg <benmemberg@gmail.com>
2018-10-27 16:14:54 -0400

    Changed folder names to be more clear

--- WhereTo/whereToHub/urls.py
+++ WhereTo/whereToHub/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('myApp/', include('myApp.urls')),
+    path('barFinder/', include('barFinder.urls')),
     path('admin/', admin.site.urls),
 ]
